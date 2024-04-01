22f1c5c1cbb5ab100adbd8bae3c246888dc03b37
frederickmannings <fred.mannings@predixus.com>
2024-04-01 14:40:28 +0100

    Rust configuration

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,3 @@
 vim.opt.relativenumber=true
 vim.opt.scrolloff=999
-
+vim.g.dap_virtual_text = true
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,3 +1,5 @@
+local cmp = require "cmp"
+
 local plugins = {
   {
     "williamboman/mason.nvim",
@@ -7,6 +9,7 @@ local plugins = {
         "lua-language-server",
         "jedi-language-server",
         "buf-language-server",
+        "rust-analyzer",
       },
     },
   },
@@ -30,6 +33,55 @@ local plugins = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = { "rust" },
+    dependencies = "neovim/nvim-lspconfig",
+    config = function()
+      require "custom.configs.rustaceanvim"
+    end
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function(_, opts)
+      local crates  = require('crates')
+      crates.setup(opts)
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+      crates.show()
+      require("core.utils").load_mappings("crates")
+    end,
+  },
+    {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      M.completion.completeopt = "menu,menuone,noselect"
+      M.mapping["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
+      }
+      table.insert(M.sources, {name = "crates"})
+      return M
     end,
   },
   {
